b74f0d435326075a23da828677b8c962ce262944
Akhil G Krishnan <akhilgkrishnan4u@gmail.com>
2020-05-01 22:35:53 +0530

    frame capturing added

--- maskdetect.py
+++ maskdetect.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2
 
+
 vid = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = vid.read()
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 vid.release()
